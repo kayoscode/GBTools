fa42ae18afb9e46c40dc9deca444ccda0ff606d0
Bryce <bryce@ubuntu.bryce>
2021-05-11 18:18:00 -0600

    initial commit

---
+++ build/CMakeFiles/GBTools.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GBTools.dir/src/main.cpp.o"
+  "GBTools"
+  "GBTools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GBTools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
